16655ce3d6a530b10f28955105c94ab3895fb270
William Sullivan <sullivanwp95@gmail.com>
2022-01-18 23:53:18 -0500

    Created consumable piece and randomly located it within play area

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+// Create player
+
 const player = document.getElementById("player")
 let direction = null;
 let x = 470;
 let y = 400;
+
+// Add movement to player
 
 document.addEventListener('keydown', function(e) {
     if (e.repeat) return;
@@ -36,3 +40,15 @@ setInterval(function() {
     player.style.left = x + "px"
     player.style.bottom = y + "px"
 }, 1)
+
+// Place consumable piece in random spot within play area
+
+function placeConsumable() {
+    let consumable = document.getElementById("consumable")
+    // https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range/1527834
+    //keeps random number in set range by utilizing the max and min possible settings
+    consumable.style.left = (Math.random() * (45.9 - 12.2) + 12.2) + "em"
+    consumable.style.bottom = (Math.random() * (41.8 - 8.2) + 8.2) + "em"
+}
+
+placeConsumable()
